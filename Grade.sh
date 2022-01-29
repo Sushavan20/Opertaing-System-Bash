9eaec70cf43f89327397319e2c721bb795ca8dd6
Sushavan Karmakar <97795991+Sushavan20@users.noreply.github.com>
2022-01-29 22:37:29 +0530

    Create Grade.sh

---
+++ Grade.sh
@@ -0,0 +1,11 @@
+printf "Enter Obtained number (out of 100): "
+read x
+printf "Grade -> "
+case $x in
+100|9[0-9]) echo "O";;
+8[0-9]) echo "E";;
+7[0-9]) echo "A";;
+6[0-9]) echo "B";;
+5[0-9]) echo "C";;
+*) echo "F";;
+esac
